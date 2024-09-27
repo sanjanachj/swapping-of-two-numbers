6e6395a4d2bd138ef5b8f2338dad6399f15a5602
sanjanachj <sanjanachoudhary717@gmail.com>
2024-09-27 13:11:13 +0530

    Add files via upload

---
+++ swap two numbers.py
@@ -0,0 +1,9 @@
+num1=int(input("enter number 1="))
+num2=int(input("enter number 2="))
+print("before swapping:")
+print("number1=",num1)
+print("number2=",num2)
+num1,num2=num2,num1
+print("after swapping")
+print("number1=",num1)
+print("number2=",num2)
